74df28244c1c96a00cb3c12266a63ef81418ac78
yakkstudios <studiosyakksol@gmail.com>
2026-03-26 22:18:04 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,20 @@
-const c={};export default c;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'unavatar.io' },
+      { protocol: 'https', hostname: 'pbs.twimg.com' },
+      { protocol: 'https', hostname: 'cdn.pump.fun' },
+      { protocol: 'https', hostname: 'arweave.net' },
+      { protocol: 'https', hostname: 'nftstorage.link' },
+    ],
+  },
+};
+
+export default nextConfig;
